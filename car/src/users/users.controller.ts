d9768f2996be547ec231bcbc5ea6ef5110e573dd
hadi arbabi <hadimojak@gmail.com>
2025-05-24 16:07:05 +0330

    fix

--- car/src/users/users.controller.ts
+++ car/src/users/users.controller.ts
@@ -2,7 +2,6 @@ import {
   Controller,
   Post,
   Get,
-  Put,
   Patch,
   Delete,
   Param,
@@ -11,13 +10,45 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './users.entity';
 
 @Controller('auth')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Post('/signup')
-  async createUser(@Body() body: CreateUserDto) {
+  async createUser(@Body() body: CreateUserDto): Promise<void> {
     await this.userService.create(body.email, body.password);
+    return;
+  }
+
+  @Get('/:id')
+  async findUserById(
+    @Param() params: { id: number },
+  ): Promise<Pick<User, 'email' | 'id' | 'password'>> {
+    return await this.userService.findOne(+params.id);
+  }
+
+  @Get('/')
+  async findUser(
+    @Query() query: { email: string },
+  ): Promise<Pick<User, 'email' | 'id' | 'password'>[]> {
+    const users = await this.userService.find(query.email);
+    return users;
+  }
+
+  @Patch('/:id')
+  async updateUser(
+    @Param() params: { id: number },
+    @Body() body: UpdateUserDto,
+  ): Promise<void> {
+    return await this.userService.update(+params.id, body);
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param() params: { id: number }): Promise<void> {
+    await this.userService.remove(+params.id);
+    return;
   }
 }
--- car/src/users/users.service.ts
+++ car/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
@@ -7,9 +7,37 @@ import { User } from './users.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  async create(email: string, password: string) {
+  async create(email: string, password: string): Promise<void> {
     const user = this.repo.create({ email, password });
+    await this.repo.save(user);
+    return;
+  }
 
-    return await this.repo.save(user);
+  async findOne(id: number): Promise<Pick<User, 'email' | 'id' | 'password'>> {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) throw new NotFoundException('user not found');
+    return user;
+  }
+
+  async find(
+    email: string,
+  ): Promise<Pick<User, 'email' | 'id' | 'password'>[]> {
+    const users = await this.repo.find({ where: { email: email } });
+    return users;
+  }
+
+  async update(id: number, attrs: Partial<User>): Promise<void> {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) throw new NotFoundException('user not found');
+    Object.assign(user, attrs);
+    await this.repo.save(user);
+    return;
+  }
+
+  async remove(id: number): Promise<void> {
+    const user = await this.repo.findOne({ where: { id } });
+    if (!user) throw new NotFoundException('user not found');
+    await this.repo.remove(user);
+    return;
   }
 }
