71fb21cf6f973925c02f9c3d55c48904aada3b15
hadi arbabi <hadimojak@gmail.com>
2025-07-14 17:08:32 +0330

    test

---
+++ car/src/users/auth.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    //crea te fake copy of usersService
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create instance of auth service', () => {
+    expect(service).toBeDefined();
+  });
+});
