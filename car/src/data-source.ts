59f7441a272b2f17e46aeebcd7276ad076ac95e6
h.arbabi <hadimojak@gmail.com>
2025-08-04 02:34:40 +0330

    before prod

--- car/src/data-source.ts
+++ car/src/data-source.ts
@@ -2,7 +2,6 @@ import { DataSource, DataSourceOptions, MigrationExecutor } from 'typeorm';
 import * as path from 'path';
 
 const baseConfig: Partial<DataSourceOptions> = {
-  type: 'sqlite',
   synchronize: false,
   migrations: [path.join(__dirname, 'migrations', '*.ts')],
   logging: false,
@@ -13,18 +12,27 @@ let environmentConfig;
 switch (process.env.NODE_ENV) {
   case 'development':
     environmentConfig = {
+      type: 'sqlite',
       database: 'db.sqlite',
       entities: [path.join(__dirname, '**', '*.entity.js')],
     };
     break;
   case 'test':
     environmentConfig = {
+      type: 'sqlite',
       database: 'test.sqlite',
       entities: [path.join(__dirname, '**', '*.entity.ts')],
       migrationsRun: true,
     };
     break;
   case 'production':
+    environmentConfig = {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [path.join(__dirname, '**', '*.entity.js')],
+      migrationsRun: true,
+      ssl: { rejectUnauthorized: false },
+    };
     break;
   default:
     throw new Error('unknown environment');
